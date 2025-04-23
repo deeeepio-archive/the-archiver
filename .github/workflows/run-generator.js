a831ccc6b759b8045cdc0f85437bca12b7374a6e
Akane Kurokawa <akanecco23@outlook.com>
2025-04-22 19:14:45 -0700

    Update scraper workflow

--- .github/workflows/run-generator.js
+++ .github/workflows/run-generator.js
@@ -11,14 +11,14 @@ concurrency:
 
 jobs:`;
 
-const template = (n) =>
+const template = (type, num) =>
 	`
-  scrape-forumPosts-${n}:
+  scrape-${type}-${num}:
     runs-on: ubuntu-latest${
-			n === 1
+			num === 1
 				? ""
 				: `
-    needs: scrape-forumPosts-${n - 1}`
+    needs: scrape-${type}-${num - 1}`
 		}
     permissions:
       contents: write
@@ -29,103 +29,50 @@ const template = (n) =>
           token: \${{ secrets.ACCESS_TOKEN }}
           ref: main
 
-      - name: Checkout forumPosts archive
+      - name: Checkout ${type} archive
         uses: actions/checkout@v4
         with:
           token: \${{ secrets.ACCESS_TOKEN }}
           ref: main
           sparse-checkout: .
-          repository: deeeepio-archive/forumPosts
-          path: forumPosts
+          repository: deeeepio-archive/${type}
+          path: ${type}
 
-      - name: Run forumPosts archive script
+      - name: Run ${type} archive script
         run: |
-          npm run scrape:forumPosts
+          npm run scrape:${type}
 
-      - name: Save changes to forumPosts archive
+      - name: Save changes to ${type} archive
         uses: stefanzweifel/git-auto-commit-action@v5
         with:
-          commit_message: Update forumPosts archive
-          repository: forumPosts
-          add_options: --sparse
-
-  scrape-maps-${n}:
-    runs-on: ubuntu-latest${
-			n === 1
-				? ""
-				: `
-    needs: scrape-maps-${n - 1}`
-		}
-    permissions:
-      contents: write
-    steps:
-      - name: Checkout scraper
-        uses: actions/checkout@v4
-        with:
-          token: \${{ secrets.ACCESS_TOKEN }}
-          ref: main
-
-      - name: Checkout maps archive
-        uses: actions/checkout@v4
-        with:
-          token: \${{ secrets.ACCESS_TOKEN }}
-          ref: main
-          sparse-checkout: .
-          repository: deeeepio-archive/maps
-          path: maps
-
-      - name: Run maps archive script
-        run: |
-          npm run scrape:maps
-
-      - name: Save changes to maps archive
-        uses: stefanzweifel/git-auto-commit-action@v5
-        with:
-          commit_message: Update maps archive
-          repository: maps
-          add_options: --sparse
-
-  scrape-users-${n}:
-    runs-on: ubuntu-latest${
-			n === 1
-				? ""
-				: `
-    needs: scrape-users-${n - 1}`
-		}
-    permissions:
-      contents: write
-    steps:
-      - name: Checkout scraper
-        uses: actions/checkout@v4
-        with:
-          token: \${{ secrets.ACCESS_TOKEN }}
-          ref: main
-
-      - name: Checkout users archive
-        uses: actions/checkout@v4
-        with:
-          token: \${{ secrets.ACCESS_TOKEN }}
-          ref: main
-          sparse-checkout: .
-          repository: deeeepio-archive/users
-          path: users
-
-      - name: Run users archive script
-        run: |
-          npm run scrape:users
-
-      - name: Save changes to users archive
-        uses: stefanzweifel/git-auto-commit-action@v5
-        with:
-          commit_message: Update users archive
-          repository: users
+          commit_message: Update ${type} archive
+          repository: ${type}
           add_options: --sparse
 `;
 
-const generateContent = (num) =>
-	header +
-	Array.from({ length: num })
-		.map((_, i) => template(i + 1))
-		.join("");
+const types = ["forumPosts", "maps", "users"];
+const generateContent = (scrapers) => {
+	let file = header;
+	const maxLength = Math.max(...Object.values(scrapers));
+	// file += Array.from({ length: num * types.length })
+	// 	.map((_, i) => template(types[i % types.length], i + 1))
+	// 	.join("");
+	for (let i = 1; i <= maxLength; i++) {
+		for (const type of types) {
+			if (scrapers[type] >= i) {
+				file += template(type, i);
+			}
+		}
+	}
+	return file;
+};
 
-fs.writeFileSync(".github/workflows/run.yml", generateContent(11), "utf-8");
+fs.writeFileSync(
+	".github/workflows/run.yml",
+	generateContent({
+		forumPosts: 12,
+		maps: 14,
+		users: 14,
+	}),
+	"utf-8",
+);
