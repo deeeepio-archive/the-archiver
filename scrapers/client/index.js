3a4f402c356b71ca52cbc50a89a79afc82d20c8f
Akane Kurokawa <akanecco23@outlook.com>
2025-05-04 12:17:28 -0700

    Add client archiver

---
+++ scrapers/client/index.js
@@ -0,0 +1,22 @@
+import fs from "node:fs";
+
+const recursiveWriteFileSync = (path, content) => {
+	const dir = path.split("/").slice(0, -1).join("/");
+	if (!fs.existsSync(dir)) {
+		fs.mkdirSync(dir, { recursive: true });
+	}
+	fs.writeFileSync(path, content);
+};
+
+const html = await (await fetch("https://deeeep.io/")).text();
+const files = [
+	/\/assets\/index\.[a-f0-9]+\.js/i,
+	/\/assets\/vendor\.[a-f0-9]+\.js/i,
+	/\/assets\/index\.[a-f0-9]+\.css/i,
+	/\/assets\/vendor\.[a-f0-9]+\.css/i,
+].map((r) => html.match(r)[0]);
+
+for (const file of files) {
+	const content = await (await fetch(`https://deeeep.io${file}`)).text();
+	recursiveWriteFileSync(`client/${file}`, content);
+}
